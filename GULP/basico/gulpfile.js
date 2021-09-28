9af7aba2aa7a57cca35100a4972ce0b0fad34c84
Silvio Félix <silvio.felix32@hotmail.com>
2021-09-28 13:58:35 -0300

    Add files via upload

---
+++ GULP/basico/gulpfile.js
@@ -0,0 +1,32 @@
+const { prependListener } = require('gulp')
+const gulp = require('gulp')
+const series = gulp.series
+
+const antes1 = cb => {
+    console.log('Tarefa antes1!')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes2!')
+    return cb()
+}
+
+function copiar(cb) {
+   /*  gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) */
+   gulp.src('pastaA/**/*.txt') // Qualquer arquivo que tenha o formato TXT vai ser copiado
+       .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(
+    antes1,
+    antes2,
+    copiar,
+    fim
+)
